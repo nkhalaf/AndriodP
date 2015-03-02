cf5a48063259b5895fd11fb7cef627a60c4a24f5
nbeel khalaf <nbeel_mofeed@hotmail.com>
2015-03-02 22:31:58 +0200

    simple  project  find the sum for tow  number and chick if the number is character or not

--- app/src/main/java/com/example/nbeel_000/nbeel/MainActivity.java
+++ app/src/main/java/com/example/nbeel_000/nbeel/MainActivity.java
@@ -4,19 +4,57 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button calculate  = (Button)findViewById(R.id.calculate);
+
+
+
+       calculate.setOnClickListener(new Button.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               EditText number1_EditT = (EditText) findViewById(R.id.number1);
+               EditText number2_EditT = (EditText) findViewById(R.id.number2);
+               EditText result_EditT = (EditText) findViewById(R.id.result);
+               String x = number1_EditT.getText().toString();
+               x = x.replaceAll(" ", "");
+               String y = number2_EditT.getText().toString();
+               y = y.replaceAll(" ", "");
+               if (isNumber(x) && isNumber(y)) {
+                   int number1 = Integer.parseInt(x);
+                   int number2 = Integer.parseInt(y);
+                   int result = number1 + number2;
+                   result_EditT.setText(result + "");
+               }
+               else{
+                   Toast.makeText(getBaseContext()," Insert wrong",Toast.LENGTH_LONG).show();
+               }
+           }
+       });
 
 
     }
 
+ public boolean isNumber(String x){
+     boolean b = true;
+    for (int i=0 ; i<x.length() ; i++){
+        if(Character.isAlphabetic(x.charAt(i))){
+            b=false;
+        }
+    }
 
+ return b;
+ }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -38,4 +76,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
